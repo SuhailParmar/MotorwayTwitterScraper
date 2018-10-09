c6e051d5e9ad6fb20edb2a5b773ed87f83134b58
zenyatta <zenyatta@Fedora>
2018-10-09 16:33:24 +0100

    safely send url

--- bin/run-dockerised-rabbit-mq.sh
+++ bin/run-dockerised-rabbit-mq.sh
@@ -12,15 +12,15 @@ echo "Waiting for Rabbit to be up..."
 sleep 20 # ToDo use wait_for it
 
 echo "Creating vhost: ${VHOST}"
-curl -i -X PUT -u guest:guest http://localhost:15672/api/vhosts/${VHOST}
+curl -i -X PUT -u guest:guest "http://localhost:15672/api/vhosts/${VHOST}"
 
 echo "Creating exchange: ${EXCHANGE}"
-curl -i -X PUT -u guest:guest http://localhost:15672/api/exchanges/${VHOST}/${EXCHANGE}
+curl -i -X PUT -u guest:guest "http://localhost:15672/api/exchanges/${VHOST}/${EXCHANGE}"
 
 echo "Creating Queue: ${M6_QUEUE}"
-curl -i -X PUT -u guest:guest http://localhost:15672/api/queues/${VHOST}/${M6_QUEUE}
+curl -i -X PUT -u guest:guest "http://localhost:15672/api/queues/${VHOST}/${M6_QUEUE}"
 
 echo "Binding Queue: ${M6_QUEUE} to exchange: ${EXCHANGE} using routing key: ${ROUTING_KEY}"
 curl -i -X POST -u guest:guest \
   --data '{"routing_key":"M6_Raw","arguments":{}}'\
-    http://localhost:15672/api/bindings/${VHOST}/e/${EXCHANGE}/q/${M6_QUEUE}
+   "http://localhost:15672/api/bindings/${VHOST}/e/${EXCHANGE}/q/${M6_QUEUE}"
